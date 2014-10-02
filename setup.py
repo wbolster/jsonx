d98515a839c8dbbed85b28efc60677b56017cb02
Wouter Bolsterlee <uws@xs4all.nl>
2014-10-02 18:53:48 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jsonx',
+    description="JSONx, an XML representation of the JSON data model"
+    version='0.1',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    py_modules=['jsonx'],
+)
