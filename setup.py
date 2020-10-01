5d635aacf3f4ffce4f5058287258862083c09205
wouter bolsterlee <wouter@bolsterl.ee>
2020-10-01 09:58:13 +0200

    bump version to 2020.10.0; use calver and party like it's 1999

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='jsonx',
     description="JSONx, an XML representation of the JSON data model",
-    version='0.1',
+    version='2020.10.0',
     author="Wouter Bolsterlee",
     author_email="uws@xs4all.nl",
     py_modules=['jsonx'],
